cd090b2430b009340f63f30c86a393cb2cb39a9c
DanlexSV <danilibra15@hotmail.com>
2022-09-21 16:14:59 +0200

    miercoles

---
+++ src/Calculadora.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Calculadora {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		long x, y, z, Suma, Resta, Multiplicación;
+		double División;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Escriba el número a: ");
+		x = sc.nextInt();
+		System.out.println("Escriba el numero b: ");
+		y = sc.nextInt();
+		
+	}
+}
---
+++ src/Ejercicio 1.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Suma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		long a, b, Suma, Resta, Multiplicación;
+		double División, Resto;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Escriba el número a: ");
+		a = sc.nextInt();
+		System.out.println("Escriba el numero b: ");
+		b = sc.nextInt();
+
+		Suma = a + b;
+		Resta = a - b;
+		Multiplicación = a * b;
+		División = a / b;
+		Resto = a % b;
+		
+		System.out.println("La suma es: " + Suma);
+		System.out.println("La resta es: " + Resta);		
+		System.out.println("La multiplicacion es: " + Multiplicación);
+		System.out.println("La división es: " + División);
+		System.out.println("El resto es: " + Resto);
+				
+	}
+
+}
