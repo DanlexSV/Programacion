30522827f8482d8af737d3805bb51e41ba25d1f4
DanlexSV <danilibra15@hotmail.com>
2022-09-24 15:07:01 +0200

    ejercicios areas

--- Proyectos Java/src/area_triangulo.java
+++ Proyectos Java/src/area_triangulo.java
@@ -1,21 +1,23 @@
 import java.util.Scanner;
 
-public class area_rectangulo {
+public class area_triangulo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		double b, h, area;
+		double b, h, multiplicacion, division;
+		int num1 = 2;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Escriba el valor de la base: ");
 		b = sc.nextInt();
 		System.out.println("Escriba el valor de la altura: ");
 		h = sc.nextInt();
 		
-		area = b * h;
+		multiplicacion = b * h;
+		division = multiplicacion / num1;
 		
-		System.out.println("El area del rectangulo es: " + area);
-		
+		System.out.println("El area del triangulo es: " + division);
+	
 	}
 
 }
