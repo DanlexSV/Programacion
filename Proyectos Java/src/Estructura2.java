c474588fa10e9b1559b2cc33456e27f8e7c5ad19
DanlexSV <danilibra15@hotmail.com>
2022-09-26 12:33:37 +0200

    estructuras

---
+++ Proyectos Java/src/Estructura2.java
@@ -0,0 +1,16 @@
+
+public class Estructura2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		if ((a >= 5) && (a <= 10)) {
+			System.out.print("El numero está entre 5 y 10");
+			
+		} else if (a > 10 || a < 5) {
+			System.out.print("El numero está fuera de rango");
+		}
+
+	}
+
+}
---
+++ Proyectos Java/src/estructuras1.java
@@ -0,0 +1,19 @@
+
+public class estructuras1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5;
+		if (a == 5) {
+			System.out.print("Es igual a 5");
+			
+		} else if (a >= 10) {
+			System.out.print("Es mayor o igual a 10");
+			
+		} else if (a < 10) {
+			System.out.print("Es menor a 10" );
+		}
+	System.out.print(" Este es el final");
+	}
+
+}
