c5b9c827a9458062e30d5fd8b6b3c33d7223ef94
DanlexSV <danilibra15@hotmail.com>
2022-09-26 11:35:46 +0200

    eliminiacion redundancia

--- Proyectos Java/src/Ejercicios.java
+++ Proyectos Java/src/Ejercicios.java
@@ -7,7 +7,7 @@ public class Ejercicios {
 		int num2 = 20;
 		int num3 = 100;
 		int multiplicacion1 = num1 * num2;
-		int division1 = multiplicacion1 / num3;
+		double division1 = multiplicacion1 / num3;
 		System.out.println("resultado 1 es: " + division1);
 		
 		int num4 = 45;
@@ -15,8 +15,7 @@ public class Ejercicios {
 		int num6 = 100;
 		double multiplicacion2 = num4 * num5;
 		double division2 = multiplicacion2 / num6;
-		double resultado2 = division2;
-		System.out.println("resultado 2 es: " + resultado2);
+		System.out.println("resultado 2 es: " + division2);
 				
 	}
 }
