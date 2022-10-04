e11498d8cd8094ca256bc6831e9aca6a0dda852e
DanlexSV <danilibra15@hotmail.com>
2022-10-04 17:34:53 +0200

    Ejercicio2 pag9

--- Proyectos Java/src/Ejercicio2_pag9.java
+++ Proyectos Java/src/Ejercicio2_pag9.java
@@ -7,8 +7,17 @@ public class Ejercicio2_pag9 {
 		
 		String menu;
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Desea ");
-
+		System.out.println("Seleccione el menu que desee consultar");
+		menu = sc.nextLine();
+		
+		switch (menu) {
+		case "Menu1": System.out.println("Revuelto de champiñiones; Filete Pollo");
+					  System.out.println("Precio: 8 Euros");break;
+		case "Menu2": System.out.println("Sopa Castellana; Chuletillas");
+					  System.out.println("Precio: 12 Euros");break;
+		case "Menu3": System.out.println("Sopa de Mariscos; Bacalao");
+					  System.out.println("Precio: 15 Euros");break;
+		default: System.out.println("No existe ese menú");
+		}
 	}
-
 }
