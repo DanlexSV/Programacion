226bb605f50cf0158ee4074025b0f5efda6ca873
DanlexSV <danilibra15@hotmail.com>
2022-10-06 09:55:49 +0200

    Trabajo2

--- Proyectos Java/src/T3_Ejercicio2_p1_pag10.java
+++ Proyectos Java/src/T3_Ejercicio2_p1_pag10.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class T3_Ejercicio1_p1_pag10 {
+public class T3_Ejercicio2_p1_pag10 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -10,14 +10,17 @@ public class T3_Ejercicio1_p1_pag10 {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Escriba un valor numérico");
 		num1 = sc.nextInt();
+		boolean primo = true;
 		
 		while (num2 <= num1) {
 			resto = num1 % num2;
 			if (resto == 0) {
-				System.out.println(num2 + " es un número divisor");num2++;
-			} else if (resto != 0) {
-				System.out.println(num2 + " no es número divisor");num2++;
+				if (num2 != 1 && num2 != num1) {
+					primo = false;
+				}				
 			}
+			num2++;
 		}
+		System.out.println("El número " + num1 + " es primo: " + primo);
 	}
 }
