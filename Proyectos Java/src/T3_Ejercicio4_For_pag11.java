843079d9ac478f53343c4f18e08c93f0f679cc18
DanlexSV <danilibra15@hotmail.com>
2022-10-07 13:27:40 +0200

    Actualizacion trabajo pag11

---
+++ Proyectos Java/src/T3_Ejercicio4_For_pag11.java
@@ -0,0 +1,13 @@
+
+public class T3_Ejercicio4_For_pag11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for (int num = 10; num <= 100; num += 10) {
+			System.out.println(num);
+		}
+		
+	}
+
+}
---
+++ Proyectos Java/src/T3_Ejercicio5_pag11.java
@@ -0,0 +1,9 @@
+
+public class T3_Ejercicio5_pag11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
