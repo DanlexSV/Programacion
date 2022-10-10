1f049aa9316926f717a0feea640c5c7083c0aab3
DanlexSV <danilibra15@hotmail.com>
2022-10-10 12:03:49 +0200

    Ejercicio5 actuaizacion

--- Proyectos Java/src/T3_Ejercicio5_pag11.java
+++ Proyectos Java/src/T3_Ejercicio5_pag11.java
@@ -3,7 +3,7 @@ public class T3_Ejercicio5_pag11 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub		
-		for (int i=4; i<4; i++) {
+		for (int i=1; i<4; i++) {
 			System.out.print(i);
 			System.out.print(" ");
 			for (int j=1; j>=1; j--)
